6444a168786025a479d9994a177ae5a29d1c94e6
Anna Yoakum <annayoakum@ufl.edu>
2019-10-13 16:12:22 -0500

    Add function to toggle menu

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // It is always helpful to use comments in your code!
 
 var productsTab = document.getElementById('products');
-var productsOptions = document.getElementById('products-list');
+var productsOptions = document.querySelector('products-list');
 
 function toggleMenu() {
   console.log(this);
+  productsOptions.classList.add('show-menu')
 }
 
 productsTab.addEventListener('mouseover', toggleMenu);
